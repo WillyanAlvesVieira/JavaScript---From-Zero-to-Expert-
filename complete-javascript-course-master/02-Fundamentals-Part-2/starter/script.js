98239bceaa7d7d81c6b34211a07f2c20c556ca43
Willyan Alves <willyan.a.vieira@outlook.com>
2024-01-04 23:00:21 -0300

    Functions Declaration and Expression (Revision)

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,63 @@
 'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = 534; uso restrito do javaScript
+ 
+*/
+
+
+/*
+//FUNCTIONS SECTION
+
+function logger(){
+   console.log('My name is Willyan');
+}
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+  const juice = ` Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(7, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2 , 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
+
+*/
+
+//FUNCTION DECLARTION
+
+const age1 = calcAge1(1991);
+
+// const calcAge1 = (birthYear) => {
+// return 2037 - birthYear 
+// }
+
+function calcAge1(birthYear){
+return 2037 - birthYear;
+}
+
+//EXPRESSION
+
+const calcAge2 = function(birthYear){
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1992)
+
+console.log(age1 , age2)
