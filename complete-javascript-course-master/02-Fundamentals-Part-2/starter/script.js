045247f982b372c68637ffca871f8e60523d6190
Willyan Alves <willyan.a.vieira@outlook.com>
2024-01-16 22:16:06 -0300

    More About Arrays

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -168,11 +168,38 @@ const friend3 = 'Peter';
 const friends = ['Michael', 'Steven', 'Peter',  ]
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(years);
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(y);
 
 console.log(friends[0]);
 console.log(friends[2]);
 
 console.log(friends.length);
-console.log(friends[friends.length -1])
+console.log(friends[friends.length -1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+//friends = ['Bob', 'Alice'] não funciona
+
+const firstName = 'Willyan'
+const willyan = [firstName, 'Alves', 2024 - 2000, 'Dev', friends];
+console.log(willyan)
+
+//Exercise
+
+const calcAge = function (birthYear){
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length -1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]),
+calcAge(years[years.length -1])];
+console.log(ages)
+
+
