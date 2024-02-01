61f8b3837260bc226aefe6256ac026099a3bc3f3
Willyan Alves <willyan.a.vieira@outlook.com>
2024-01-31 23:17:58 -0300

    DOM MANIPULATION AND GUESS MY NUMBER !

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent); // before change
+document.querySelector('.message').textContent = 'Correct Number!'
+console.log(document.querySelector('.message').textContent); //after change 
+
+document.querySelector('.number').textContent = 14;
+document.querySelector('.score').textContent = 12;
+
+document.querySelector('.guess').value = 23,
+console.log(document.querySelector('.guess').value);
