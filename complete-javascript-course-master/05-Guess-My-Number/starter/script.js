26f7a805ca9370231538a1b48d8f64c2fa479d84
Willyan Alves <willyan.a.vieira@outlook.com>
2024-02-01 23:31:36 -0300

    AddEventListener and Start The Project

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent); // before change
-document.querySelector('.message').textContent = 'Correct Number!'
-console.log(document.querySelector('.message').textContent); //after change 
+// console.log(document.querySelector('.message').textContent); // before change
+// document.querySelector('.message').textContent = 'Correct Number!'
+// console.log(document.querySelector('.message').textContent); //after change 
 
-document.querySelector('.number').textContent = 14;
-document.querySelector('.score').textContent = 12;
+// document.querySelector('.number').textContent = 14;
+// document.querySelector('.score').textContent = 12;
 
-document.querySelector('.guess').value = 23,
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23,
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', 
+function(){
+    console.log(document.querySelector('.guess').value);
+    document.querySelector('.message').textContent = 'Correct Number!'
+
+})
